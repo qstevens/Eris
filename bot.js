a27f35a8a4a5442e89d78c65062019b6b381335f
Stevens Qiu <stevensqiu@gmail.com>
2018-05-15 12:54:20 -0700

    refactor several functions

--- bot.js
+++ bot.js
@@ -16,31 +16,62 @@ bot.on("ready", async () => {
     console.log(e.stack);
   }
 
-  fs.readFile('plans.json', (err, data) => {
-    if (err) throw err;
-    let newPlans = JSON.parse(data);
+  setTimerJSON();
 
-    var JSONArray = newPlans.plans;
+  function setTimerJSON(x = 0) {
+    fs.readFile('plans.json', (err, data) => {
+      if (err) throw err;
+      let newPlans = JSON.parse(data);
 
-    for (i = 0; i < JSONArray.length; i++) {
-      var eventInfo = JSONArray[i]
-      console.log(eventInfo);
-      var eventDate = eventInfo.date;
-      var timeToPlan = Date.parse(eventDate) - new Date().getTime();
+      var JSONArray = newPlans.plans;
 
-      setTimeout(function(err) {
-        if(err) return console.log(err);
-        if (Date.parse(eventDate) - new Date().getTime() < 10000) {
-          botChannel = bot.channels.get(eventInfo.channel)
-          botChannel.send(eventInfo.name + " " + "is coming up (" + eventInfo.strDate + ")");
-        }
-      },
-      timeToPlan);
-      console.log(timeToPlan);
+      for (i = 0; i < JSONArray.length; i++) {
+        var eventInfo = JSONArray[i];
+        console.log(eventInfo);
+        var eventDate = eventInfo.date;
+        var timeUntilEvent = Date.parse(eventDate) - new Date().getTime();
+
+        setTimeout(function(err) {
+          if (err) return console.log(err);
+          if (Date.parse(eventDate) - new Date().getTime() < 10000) {
+            botChannel = bot.channels.get(eventInfo.channel)
+            botChannel.send(eventInfo.name + " is coming up (" +
+            eventInfo.strDate + ")");
+          }
+        },
+      timeUntilEvent);
+      console.log(timeUntilEvent);
       JSONArray.splice(i);
-    }
-    fs.writeFileSync("plans.json", JSON.stringify(newPlans, null, 2));
-  });
+      }
+      fs.writeFileSync('plans.json', JSON.stringify(newPlans, null, 2));
+    })
+  }
+
+  // fs.readFile('plans.json', (err, data) => {
+  //   if (err) throw err;
+  //   let newPlans = JSON.parse(data);
+  //
+  //   var JSONArray = newPlans.plans;
+  //
+  //   for (i = 0; i < JSONArray.length; i++) {
+  //     var eventInfo = JSONArray[i]
+  //     console.log(eventInfo);
+  //     var eventDate = eventInfo.date;
+  //     var timeToPlan = Date.parse(eventDate) - new Date().getTime();
+  //
+  //     setTimeout(function(err) {
+  //       if(err) return console.log(err);
+  //       if (Date.parse(eventDate) - new Date().getTime() < 10000) {
+  //         botChannel = bot.channels.get(eventInfo.channel)
+  //         botChannel.send(eventInfo.name + " " + "is coming up (" + eventInfo.strDate + ")");
+  //       }
+  //     },
+  //     timeToPlan);
+  //     console.log(timeToPlan);
+  //     JSONArray.splice(i);
+  //   }
+  //   fs.writeFileSync("plans.json", JSON.stringify(newPlans, null, 2));
+  // });
 
 });
 
@@ -120,63 +151,103 @@ bot.on("message", async message => {
           setMin = +time.substring(timeLen-2, timeLen);
         }
 
-        if (ampm.charAt(0).toUpperCase() === "P") {
+        if (ampm.charAt(0).toUpperCase() === "P" && setHours != 12) {
           setHours += 12;
         }
 
         var setMonth;
 
-        switch (month.toUpperCase().substring(0, 3)) {
-          case "JAN":
-            setMonth = 0;
-            break;
-          case "FEB":
-            setMonth = 1;
-            break;
-          case "MAR":
-            setMonth = 2;
-            break;
-          case "APR":
-            setMonth = 3;
-            break;
-          case "MAY":
-            setMonth = 4;
-            break;
-          case "JUN":
-            setMonth = 5;
-            break;
-          case "JUL":
-            setMonth = 6;
-            break;
-          case "AUG":
-            setMonth = 7;
-            break;
-          case "SEP":
-            setMonth = 8;
-            break;
-          case "OCT":
-            setMonth = 9;
-            break;
-          case "NOV":
-            setMonth = 10;
-            break;
-          case "DEC":
-            setMonth = 11;
-            break;
+        setMonthNum();
+
+        function setMonthNum() {
+          switch (month.toUpperCase().substring(0, 3)) {
+            case "JAN":
+              setMonth = 0;
+              break;
+            case "FEB":
+              setMonth = 1;
+              break;
+            case "MAR":
+              setMonth = 2;
+              break;
+            case "APR":
+              setMonth = 3;
+              break;
+            case "MAY":
+              setMonth = 4;
+              break;
+            case "JUN":
+              setMonth = 5;
+              break;
+            case "JUL":
+              setMonth = 6;
+              break;
+            case "AUG":
+              setMonth = 7;
+              break;
+            case "SEP":
+              setMonth = 8;
+              break;
+            case "OCT":
+              setMonth = 9;
+              break;
+            case "NOV":
+              setMonth = 10;
+              break;
+            case "DEC":
+              setMonth = 11;
+              break;
+          }
         }
 
-        var currentDate = new Date();
-        var currentYear = currentDate.getFullYear();
-        var currentMonth = currentDate.getMonth();
-        var currentDay = currentDate.getDate();
-        var currentHours = currentDate.getHours();
-        var currentMin = currentDate.getMinutes();
-        var currentSec = currentDate.getSeconds();
+        // switch (month.toUpperCase().substring(0, 3)) {
+        //   case "JAN":
+        //     setMonth = 0;
+        //     break;
+        //   case "FEB":
+        //     setMonth = 1;
+        //     break;
+        //   case "MAR":
+        //     setMonth = 2;
+        //     break;
+        //   case "APR":
+        //     setMonth = 3;
+        //     break;
+        //   case "MAY":
+        //     setMonth = 4;
+        //     break;
+        //   case "JUN":
+        //     setMonth = 5;
+        //     break;
+        //   case "JUL":
+        //     setMonth = 6;
+        //     break;
+        //   case "AUG":
+        //     setMonth = 7;
+        //     break;
+        //   case "SEP":
+        //     setMonth = 8;
+        //     break;
+        //   case "OCT":
+        //     setMonth = 9;
+        //     break;
+        //   case "NOV":
+        //     setMonth = 10;
+        //     break;
+        //   case "DEC":
+        //     setMonth = 11;
+        //     break;
+        // }
 
         var setDate = new Date(setYear, setMonth, setDay,
           setHours, setMin, 0);
 
-        var date = month + " " + setDay + " " + time + " " + ampm;
+        var date = month.toUpperCase() + " " + setDay + " " + time + " " +
+        ampm.toUpperCase();
+
+        console.log(setDay);
+        console.log(setHours);
+        console.log(new Date().getTime());
 
         var diffTimeDays = setDate.getTime() - new Date().getTime();
 
@@ -186,26 +257,48 @@ bot.on("message", async message => {
           name += " " + args[i];
         }
 
-        fs.readFile('plans.json', (err, data) => {
-          if (err) throw err;
-          let jsonPlans = JSON.parse(data);
-          var JSONArray = jsonPlans.plans;
+        writeTimerJSON()
 
-          for (i = 0; i < JSONArray.length; i++) {
-            if (JSONArray[i].name.toUpperCase === name.toUpperCase) {
-              JSONArray.splice(i);
+        function writeTimerJSON() {
+          fs.readFile('plans.json', (err, data) => {
+            if (err) throw err;
+            let jsonPlans = JSON.parse(data);
+            var JSONArray = jsonPlans.plans;
+
+            for (i = 0; i < JSONArray.length; i++) {
+              if (JSONArray[i].name.toUpperCase === name.toUpperCase) {
+                JSONArray.splice(i);
+              }
             }
-          }
-          jsonPlans.plans.push({
-            name: name,
-            date: setDate,
-            strDate: date,
-            channel: message.channel.id,
-          })
-          fs.writeFileSync("plans.json", JSON.stringify(jsonPlans, null, 2));
-        });
+            jsonPlans.plans.push({
+              name: name,
+              date: setDate,
+              strDate: date,
+              channel: message.channel.id,
+            })
+            fs.writeFileSync('plans.json', JSON.stringify(jsonPlans, null, 2));
+          });
+          message.channel.send(name + " has been set for " + date);
+        }
 
-        message.channel.send(name + " has been set for " + date);
+        // fs.readFile('plans.json', (err, data) => {
+        //   if (err) throw err;
+        //   let jsonPlans = JSON.parse(data);
+        //   var JSONArray = jsonPlans.plans;
+        //
+        //   for (i = 0; i < JSONArray.length; i++) {
+        //     if (JSONArray[i].name.toUpperCase === name.toUpperCase) {
+        //       JSONArray.splice(i);
+        //     }
+        //   }
+        //   jsonPlans.plans.push({
+        //     name: name,
+        //     date: setDate,
+        //     strDate: date,
+        //     channel: message.channel.id,
+        //   })
+        //   fs.writeFileSync("plans.json", JSON.stringify(jsonPlans, null, 2));
+        // });
 
         setTimeout(function(err) {
           if(err) return console.log(err);
